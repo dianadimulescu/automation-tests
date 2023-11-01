36d2285ba849e47cc4ebb375bdf8314d9bc83b4d
dianadimulescu <diana_dimulescu@yahoo.com>
2023-11-01 10:52:33 +0200

    Add delete post test

--- tests/delete-post.js
+++ tests/delete-post.js
@@ -1,10 +1,10 @@
 const { spec } = require('pactum');
+const getUrl = 'https://reqres.in';
 
-describe('Delete post', () => {
-    it('DELETE', async() => {
-        const getUrl = 'https://reqres.in'
+describe('Delete post suite test', () => {
+    it('DELETE post', async() => {
         await spec()
-        .delete(getUrl + '/api/users/2')
-        .expectStatus(204);
+            .delete(getUrl + '/api/users/2')
+            .expectStatus(204);
     });
 });
